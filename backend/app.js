8198261133626affef3ae3b8a310f2d6ff96411b
Yerkhan Bayanov <yerkhan.bayanov@gmail.com>
2024-06-30 18:56:52 +0200

    cors

--- backend/app.js
+++ backend/app.js
@@ -13,41 +13,11 @@ const app = express();
 config({ path: "./config/config.env" });
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL, process.env.NETLIFY_FRONTEND_URL],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
 }));
 
-const allowedOrigins = [
-  'https://6680a26de36167717b13f95d--dulcet-churros-9aeffe.netlify.app',
-  'https://dulcet-churros-9aeffe.netlify.app'
-];
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigins);
-  next();
-});
-
-
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
