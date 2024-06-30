f496b7b2b796a2a175d6508ea50b34bc615eb109
Yerkhan Bayanov <yerkhan.bayanov@gmail.com>
2024-06-30 02:24:32 +0200

    https

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -2,14 +2,16 @@ export const generateToken = (user, message, statusCode, res) => {
     const token = user.generateJsonWebToken();
 
     const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
-  
+    res.header('Access-Control-Allow-Origin', 'https://6680a26de36167717b13f95d--dulcet-churros-9aeffe.netlify.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res
       .status(statusCode)
       .cookie(cookieName, token, {
         expires: new Date(
           Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
+        httpOnly: true,httpsOnly: true,
       })
       .json({
         success: true,
